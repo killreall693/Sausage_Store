84b8bdbc68a1290142ec3b60c3db66f7737f569c
Stasyan <stasyan@120003.ru>
2021-12-01 10:50:21 +0000

    Revert "Added MongoDB integration"
    This reverts commit 2b945d6a3bf81960e1ac2b3b8e77c09b67549ef2

--- backend/src/main/java/com/yandex/practicum/devops/SausageApplication.java
+++ backend/src/main/java/com/yandex/practicum/devops/SausageApplication.java
@@ -6,10 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class SausageApplication {
 
     public static void main(String[] args) {
